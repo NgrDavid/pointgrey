NgrDavid/mixer

Add CI/CD and brought repository into compliance with modern standards

docs/template/public/main.js
@@ -1,0 +1,13 @@
+import WorkflowContainer from "./workflow.js"
+
+export default {
+    defaultTheme: 'light',
+    iconLinks: [{
+        icon: 'github',
+        href: 'https://github.com/bonsai-rx/mixer',
+        title: 'GitHub'
+    }],
+    start: () => {
+        WorkflowContainer.init();
+    }
+}

NgrDavid/pointgrey

docs/template/public/main.js
@@ -1,0 +1,13 @@
+import WorkflowContainer from "./workflow.js"
+
+export default {
+    defaultTheme: 'light',
+    iconLinks: [{
+        icon: 'github',
+        href: 'https://github.com/bonsai-rx/pointgrey',
+        title: 'GitHub'
+    }],
+    start: () => {
+        WorkflowContainer.init();
+    }
+}